52cb06366e0b5d90af6299add00a4f8afecf472f
moises arias <moises01arias@gmail.com>
2021-10-14 22:12:37 -0300

    14/10

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    collection do
+      get 'top'
+    end
+    resources :reviews, only: %i[new create]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+c = "chinese"
+it = "italian"
+ja = "japanese"
+fr = "french"
+be = "belgian"
+
+cate = [c, it, ja, fr, be]
+
+10.times do
+  rest = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: cate.sample
+  )
+  rev = Review.new(
+    content: "good",
+    rating: (1..5).to_a.sample
+  )
+  rev.restaurant = rest
+  rev.save
+end
+puts "Finished!"
