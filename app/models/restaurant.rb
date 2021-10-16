a27ebdc92a8288ec08378d84e082ef567f575c03
moises arias <moises01arias@gmail.com>
2021-10-16 15:31:06 -0300

    16/10

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  CATEGORIES = %w[chinese italian japanese french belgian].freeze
+  CATEGORIES = %w[Chinese Italian Japanese French Belgian].freeze
   validates :category, inclusion: { in: CATEGORIES }
+  validates :name, presence: true
+  validates :address, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :rating, numericality: { greater_than: 0 }
+  validates :rating, numericality: { less_than: 6 }
+  validates :content, presence: true
 end
